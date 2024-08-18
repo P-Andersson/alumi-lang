8b22f7538c11d0d95b067f2face29c9719e2afb7
Pierre Andersson <pierre.andersson01@gmail.com>
2024-08-18 02:00:58 +0200

    add tuple utils

---
+++ lib/alccemy/test/src/util/test_tuple.cpp
@@ -0,0 +1,61 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <utf8cpp/utf8.h>
+
+#include <tuple>
+#include <string>
+
+
+import alccemy.util.tuple;
+
+using namespace alccemy;
+
+
+TEST_CASE("Test Tuple For Each")
+{
+	SECTION("Basic")
+	{
+		std::tuple<std::string, int, bool> tuple("test", 42, false);
+
+		int iterations = 0;
+		tuple_for_each(tuple, [&](const auto& val) {
+			using ValT = std::decay_t<decltype(val)>;
+			iterations += 1;
+			if constexpr (std::same_as<std::string, ValT>) {
+				REQUIRE(val == "test");
+			} else if constexpr (std::same_as<int, ValT>) {
+				REQUIRE(val == 42);
+			} else if constexpr (std::same_as<bool, ValT>) {
+				REQUIRE(val == false);
+			}
+		});
+
+		REQUIRE(iterations == 3);
+	}
+
+	SECTION("Mutations")
+	{
+		std::tuple<std::string, int, bool> tuple("test", 42, false);
+
+		int iterations = 0;
+		tuple_for_each(tuple, [&](auto& val) {
+			using ValT = std::decay_t<decltype(val)>;
+			iterations += 1;
+			if constexpr (std::same_as<std::string, ValT>) {
+				val += "_mutated";
+			}
+			else if constexpr (std::same_as<int, ValT>) {
+				val += 2;
+			}
+			else if constexpr (std::same_as<bool, ValT>) {
+				val = !val;
+			}
+		});
+
+		REQUIRE(iterations == 3);
+
+		REQUIRE(std::get<0>(tuple) == "test_mutated");
+		REQUIRE(std::get<1>(tuple) == 44);
+		REQUIRE(std::get<2>(tuple) == true);
+	}
+}
